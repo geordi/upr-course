ff33cab897d51087dffba5002d297078a891e101
Daniel Trnka <daniel.trnka@gmail.com>
2019-10-19 23:01:41 +0200

    template: each test is in separate method so we can comment some test more easily

--- tasks/template/array.c
+++ tasks/template/array.c
@@ -31,137 +31,127 @@ int dot(const int *a, const int *b, const int len) {
 void counting_sort(int *array, const int len, const int from, const int to) {
 }
 
-/**
- * Funkce otestuje, ze jsou Vase funkce spravne naimplementovany
- * kodu neni potreba uplne rozumet
- *
- * v pripade chybne implementace funkce se vypise napr:
- * array_1d: array_1d.c:168: run_tests: Assertion `memcmp(nums, expected, sizeof(expected)) == 0' failed.
- * test na radku 168 neprosel a je nutne zjistit proc
- *  - vypsat si hodnotu napr. pomoci printf - v tomto pripade print_array(nums, sizeof(nums) / sizeof(*nums));
- *  - podivat se na hodnotu nums v debuggeru
- */
-void run_tests() {
-  // fill array
+void test_fill_array() {
+  int nums[6];
+  int expected[] = {10, 15, 20, 25, 30, 35};
+
+  fill_array(nums, sizeof(nums) / sizeof(*nums), 10, 5);
+
+  // compare that two arrays are equal
+  assert(memcmp(nums, expected, sizeof(expected)) == 0);
+
+  // test we can pass NULL pointer
+  fill_array(NULL, 10, 0, 0);
+}
+
+void test_num_count() {
+  int nums[] = {10, 20, 10, 30};
+
+  assert(num_count(nums, sizeof(nums) / sizeof(*nums), 10) == 2);
+  assert(num_count(nums, sizeof(nums) / sizeof(*nums), 20) == 1);
+  assert(num_count(nums, sizeof(nums) / sizeof(*nums), 200) == 0);
+  assert(num_count(nums, 0, 200) == 0);
+}
+
+void test_in_interval() {
+  int nums[] = {10, 20, 10, 30};
+
+  assert(in_interval(nums, sizeof(nums) / sizeof(*nums), 10, 30) == 4);
+  assert(in_interval(nums, sizeof(nums) / sizeof(*nums), 10, 29) == 3);
+  assert(in_interval(nums, sizeof(nums) / sizeof(*nums), 11, 29) == 1);
+  assert(in_interval(nums, sizeof(nums) / sizeof(*nums), 100, 200) == 0);
+}
+
+void test_avg() {
   {
-    int nums[6];
-    int expected[] = {10, 15, 20, 25, 30, 35};
+    int nums[] = {10, 10, 10};
+    assert(avg(nums, sizeof(nums) / sizeof(*nums)) == 10.0);
+  }
 
-    fill_array(nums, sizeof(nums) / sizeof(*nums), 10, 5);
+  {
+    int nums[] = {10, 20};
+    assert(avg(nums, sizeof(nums) / sizeof(*nums)) == 15.0);
+  }
+  {
+    int nums[] = {-10, 10};
+    assert(avg(nums, sizeof(nums) / sizeof(*nums)) == 0);
+  }
+  {
+    int nums[] = {1, 4, 5};
+    assert(fabs(avg(nums, sizeof(nums) / sizeof(*nums)) - 3.33333) < 0.00001);
+  }
+}
 
-    // compare that two arrays are equal
+void test_min_max() {
+  int nums[] = {10, 10, 20, 10, 30, 5, 10};
+  int min = -1, max = -1;
+
+  min_max(nums, sizeof(nums) / sizeof(*nums), &min, &max);
+  assert(min == 5 && max == 30);
+
+  // test two first elements
+  min_max(nums, 2, &min, &max);
+  assert(min == 10 && max == 10);
+
+  // try to pass NULLS to pointers
+  min_max(NULL, 1, &min, &max);
+  min_max(nums, 1, NULL, &max);
+  min_max(nums, 1, &min, NULL);
+}
+
+void test_array_reverse() {
+  int nums[] = {10, 20, 30, 40, 50};
+  int expected[] = {50, 40, 30, 20, 10};
+
+  array_reverse(nums, sizeof(nums) / sizeof(*nums));
+  assert(memcmp(nums, expected, sizeof(expected)) == 0);
+}
+
+void test_dot() {
+  {
+    int a[] = {10, 20};
+    int b[] = {20, -10};
+    assert(dot(a, b, 2) == 0);
+  }
+  {
+    int a[] = {10, 20};
+    int b[] = {10, 20};
+    assert(dot(a, b, 2) == 500);
+  }
+}
+
+void test_counting_sort() {
+  {
+    int nums[] = {5, 4, 4, 3, 2, 2};
+    int expected[] = {2, 2, 3, 4, 4, 5};
+
+    counting_sort(nums, sizeof(nums) / sizeof(*nums), 0, 5);
+    assert(memcmp(nums, expected, sizeof(expected)) == 0);
+  }
+
+  {
+    int nums[] = {5, 4, 4, 3, 2, 2};
+    int expected[] = {2, 2, 3, 4, 4, 5};
+
+    // use only counters for 2, 3, 4, 5
+    counting_sort(nums, sizeof(nums) / sizeof(*nums), 2, 5);
     assert(memcmp(nums, expected, sizeof(expected)) == 0);
 
-    // test we can pass NULL pointer
-    fill_array(NULL, 10, 0, 0);
+    // check that we have counter for all numbers in array
+    counting_sort(nums, sizeof(nums) / sizeof(*nums), 0, 1);
   }
-
-  // num_count
-  {
-    int nums[] = {10, 20, 10, 30};
-
-    assert(num_count(nums, sizeof(nums) / sizeof(*nums), 10) == 2);
-    assert(num_count(nums, sizeof(nums) / sizeof(*nums), 20) == 1);
-    assert(num_count(nums, sizeof(nums) / sizeof(*nums), 200) == 0);
-    assert(num_count(nums, 0, 200) == 0);
-  }
-
-  // in_interval
-  {
-    int nums[] = {10, 20, 10, 30};
-
-    assert(in_interval(nums, sizeof(nums) / sizeof(*nums), 10, 30) == 4);
-    assert(in_interval(nums, sizeof(nums) / sizeof(*nums), 10, 29) == 3);
-    assert(in_interval(nums, sizeof(nums) / sizeof(*nums), 11, 29) == 1);
-    assert(in_interval(nums, sizeof(nums) / sizeof(*nums), 100, 200) == 0);
-  }
-
-  // avg
-  {
-    {
-      int nums[] = {10, 10, 10};
-      assert(avg(nums, sizeof(nums) / sizeof(*nums)) == 10.0);
-    }
-
-    {
-      int nums[] = {10, 20};
-      assert(avg(nums, sizeof(nums) / sizeof(*nums)) == 15.0);
-    }
-    {
-      int nums[] = {-10, 10};
-      assert(avg(nums, sizeof(nums) / sizeof(*nums)) == 0);
-    }
-    {
-      int nums[] = {1, 4, 5};
-      assert(fabs(avg(nums, sizeof(nums) / sizeof(*nums)) - 3.33333) < 0.00001);
-    }
-  }
-
-  { // min_max
-    int nums[] = {10, 10, 20, 10, 30, 5, 10};
-    int min = -1, max = -1;
-
-    min_max(nums, sizeof(nums) / sizeof(*nums), &min, &max);
-    assert(min == 5 && max == 30);
-
-    // test two first elements
-    min_max(nums, 2, &min, &max);
-    assert(min == 10 && max == 10);
-
-    // try to pass NULLS to pointers
-    min_max(NULL, 1, &min, &max);
-    min_max(nums, 1, NULL, &max);
-    min_max(nums, 1, &min, NULL);
-  }
-
-  { // array_reverse
-    int nums[] = {10, 20, 30, 40, 50};
-    int expected[] = {50, 40, 30, 20, 10};
-
-    array_reverse(nums, sizeof(nums) / sizeof(*nums));
-    assert(memcmp(nums, expected, sizeof(expected)) == 0);
-  }
-
-  { // dot
-    {
-      int a[] = {10, 20};
-      int b[] = {20, -10};
-      assert(dot(a, b, 2) == 0);
-    }
-    {
-      int a[] = {10, 20};
-      int b[] = {10, 20};
-      assert(dot(a, b, 2) == 500);
-    }
-  }
-
-  { // counting_sort
-    {
-      int nums[] = {5, 4, 4, 3, 2, 2};
-      int expected[] = {2, 2, 3, 4, 4, 5};
-
-      counting_sort(nums, sizeof(nums) / sizeof(*nums), 0, 5);
-      assert(memcmp(nums, expected, sizeof(expected)) == 0);
-    }
-
-    {
-      int nums[] = {5, 4, 4, 3, 2, 2};
-      int expected[] = {2, 2, 3, 4, 4, 5};
-
-      // use only counters for 2, 3, 4, 5
-      counting_sort(nums, sizeof(nums) / sizeof(*nums), 2, 5);
-      assert(memcmp(nums, expected, sizeof(expected)) == 0);
-
-      // check that we have counter for all numbers in array
-      counting_sort(nums, sizeof(nums) / sizeof(*nums), 0, 1);
-    }
-  }
-
-  printf("OK\n");
 }
 
 int main() {
-  run_tests();
+  test_fill_array();
+  test_num_count();
+  test_in_interval();
+  test_avg();
+  test_min_max();
+  test_array_reverse();
+  test_dot();
+  test_counting_sort();
 
   return 0;
 }
+
--- tasks/template/array_2d.c
+++ tasks/template/array_2d.c
@@ -35,112 +35,99 @@ void matrix_mul_scalar(int* matrix, const int rows, const int cols, const int k)
 int* matrix_mul_vec(const int* matrix, const int rows, const int cols, const int *vec, const int len) {
 }
 
-/**
- * Funkce otestuje, ze jsou Vase funkce spravne naimplementovany
- * kodu neni potreba uplne rozumet
- *
- * v pripade chybne implementace funkce se vypise napr:
- * array_1d: array_1d.c:168: run_tests: Assertion `memcmp(nums, expected, sizeof(expected)) == 0' failed.
- * test na radku 168 neprosel a je nutne zjistit proc
- *  - vypsat si hodnotu napr. pomoci printf - v tomto pripade print_array(nums, sizeof(nums) / sizeof(*nums));
- *  - podivat se na hodnotu nums v debuggeru
- */
-void run_tests() {
-  {
-    int expected[] = {
-      'X', ' ', ' ', ' ', ' ', 'X', ' ', ' ', ' ', ' ', 'X',
-      ' ', 'X', ' ', ' ', ' ', 'X', ' ', ' ', ' ', 'X', ' ',
-      ' ', ' ', 'X', ' ', ' ', 'X', ' ', ' ', 'X', ' ', ' ',
-      ' ', ' ', ' ', 'X', ' ', 'X', ' ', 'X', ' ', ' ', ' ',
-      ' ', ' ', ' ', ' ', 'X', 'X', 'X', ' ', ' ', ' ', ' ',
-      'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X',
-      ' ', ' ', ' ', ' ', 'X', 'X', 'X', ' ', ' ', ' ', ' ',
-      ' ', ' ', ' ', 'X', ' ', 'X', ' ', 'X', ' ', ' ', ' ',
-      ' ', ' ', 'X', ' ', ' ', 'X', ' ', ' ', 'X', ' ', ' ',
-      ' ', 'X', ' ', ' ', ' ', 'X', ' ', ' ', ' ', 'X', ' ',
-      'X', ' ', ' ', ' ', ' ', 'X', ' ', ' ', ' ', ' ', 'X',
-    };
+void test_draw_star() {
+  int expected[] = {
+    'X', ' ', ' ', ' ', ' ', 'X', ' ', ' ', ' ', ' ', 'X',
+    ' ', 'X', ' ', ' ', ' ', 'X', ' ', ' ', ' ', 'X', ' ',
+    ' ', ' ', 'X', ' ', ' ', 'X', ' ', ' ', 'X', ' ', ' ',
+    ' ', ' ', ' ', 'X', ' ', 'X', ' ', 'X', ' ', ' ', ' ',
+    ' ', ' ', ' ', ' ', 'X', 'X', 'X', ' ', ' ', ' ', ' ',
+    'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X',
+    ' ', ' ', ' ', ' ', 'X', 'X', 'X', ' ', ' ', ' ', ' ',
+    ' ', ' ', ' ', 'X', ' ', 'X', ' ', 'X', ' ', ' ', ' ',
+    ' ', ' ', 'X', ' ', ' ', 'X', ' ', ' ', 'X', ' ', ' ',
+    ' ', 'X', ' ', ' ', ' ', 'X', ' ', ' ', ' ', 'X', ' ',
+    'X', ' ', ' ', ' ', ' ', 'X', ' ', ' ', ' ', ' ', 'X',
+  };
 
 
-    int N = 11;
-    int *img = (int*) malloc(sizeof(int) * N * N);
+  int N = 11;
+  int *img = (int*) malloc(sizeof(int) * N * N);
 
-    // vymazani obrazku
-    for(int i = 0; i < N * N; i++) {
-      img[i] = ' ';
-    }
-
-    draw_star(img, N, N);
-    print_img(img, N, N);
-    assert(memcmp(img, expected, sizeof(expected)) == 0);
-
-    free(img);
+  // vymazani obrazku
+  for(int i = 0; i < N * N; i++) {
+    img[i] = ' ';
   }
 
-  {
-    {
-      int matrix[] = {
-        3, 5, 7,
-        1, 2, 8,
-        2, 3, 1
-      };
+  draw_star(img, N, N);
+  print_img(img, N, N);
+  assert(memcmp(img, expected, sizeof(expected)) == 0);
 
-      int k = 4;
-      int expected[] = {
-        3*k, 5*k, 7*k,
-        1*k, 2*k, 8*k,
-        2*k, 3*k, 1*k
-      };
+  free(img);
+}
 
-      matrix_mul_scalar(matrix, 3, 3, k);
-      assert(memcmp(matrix, expected, sizeof(expected)) == 0);
-    }
-    {
-      int matrix[] = {
-        3, 5, 7,
-        1, 2, 8,
-      };
-
-      int k = 4;
-      int expected[] = {
-        3*k, 5*k, 7*k,
-        1*k, 2*k, 8*k,
-      };
-
-      matrix_mul_scalar(matrix, 2, 3, k);
-      assert(memcmp(matrix, expected, sizeof(expected)) == 0);
-    }
-  }
-
+void test_matrix_mul_scalar() {
   {
     int matrix[] = {
-      1, 2, 3,
-      4, 5, 6,
-      7, 8, 9,
+      3, 5, 7,
+      1, 2, 8,
+      2, 3, 1
     };
-    int vector[] = {10, 20, 30};
-    int expected[] = {140, 320, 500};
 
+    int k = 4;
+    int expected[] = {
+      3*k, 5*k, 7*k,
+      1*k, 2*k, 8*k,
+      2*k, 3*k, 1*k
+    };
 
-    printf("======= matrix_mul_vec =======\n");
-    print_matrix(matrix, 3, 3);
-    printf("multiply by: \n");
-    print_matrix(vector, 3, 1);
-
-    int* result = matrix_mul_vec(matrix, 3, 3, vector, 3);
-    assert(result);
-    printf("result: \n");
-    print_matrix(result, 3, 1);
-    assert(memcmp(result, expected, sizeof(expected)) == 0);
-
-    free(result);
+    matrix_mul_scalar(matrix, 3, 3, k);
+    assert(memcmp(matrix, expected, sizeof(expected)) == 0);
   }
+  {
+    int matrix[] = {
+      3, 5, 7,
+      1, 2, 8,
+    };
 
-  printf("OK\n");
+    int k = 4;
+    int expected[] = {
+      3*k, 5*k, 7*k,
+      1*k, 2*k, 8*k,
+    };
+
+    matrix_mul_scalar(matrix, 2, 3, k);
+    assert(memcmp(matrix, expected, sizeof(expected)) == 0);
+  }
+}
+
+void test_matrix_mul_vec() {
+  int matrix[] = {
+    1, 2, 3,
+    4, 5, 6,
+    7, 8, 9,
+  };
+  int vector[] = {10, 20, 30};
+  int expected[] = {140, 320, 500};
+
+
+  printf("======= matrix_mul_vec =======\n");
+  print_matrix(matrix, 3, 3);
+  printf("multiply by: \n");
+  print_matrix(vector, 3, 1);
+
+  int* result = matrix_mul_vec(matrix, 3, 3, vector, 3);
+  assert(result);
+  printf("result: \n");
+  print_matrix(result, 3, 1);
+  assert(memcmp(result, expected, sizeof(expected)) == 0);
+
+  free(result);
 }
 
 int main() {
-  run_tests();
-
+  test_draw_star();
+  test_matrix_mul_scalar();
+  test_matrix_mul_vec();
   return 0;
 }
